873d552942884860f8157ea25418d4f173d06b93
srobertson421 <srobertson421@gmail.com>
2019-12-09 21:01:11 -0800

    initial db menu creation hook

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,13 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.onMenuCreate = functions
+.firestore
+.document('/businesses/{businessId}/{menusCollectionId}/{menuId}')
+.onCreate((snap, context) => {
+  const menuData = snap.data();
+  const businessId = snap.ref.parent.parent.id;
+  console.log(menuData, businessId);
+  return null;
+});
